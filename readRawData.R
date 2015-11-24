9076490afee2ae5219096ea3caaf39f539007cec
Ray <zhirui1992@gmail.com>
2015-11-24 12:25:27 -0500

    add sample data, prepare for building model

--- readRawData.R
+++ readRawData.R
@@ -7,10 +7,37 @@ movie_info <- read.csv("movie_titles_preprocessed.txt",sep="=")
 movie_info[[1]] <- NULL
 library(RCurl)
 
+#calculate the string distance, and then cluster the movie based on the strin distance
 library(stringdist)
 distanceMovies <- stringdistmatrix(movie_info$name,movie_info$name,method = "jw")
 hc <- hclust(as.dist(distanceMovies))
 dfClust <- data.frame(movie_info$name, cutree(hc, k = 1000))
 names(dfClust) <- c('movie_name','cluster')
 plot(table(dfClust$cluster))
+
+#Create sample training and testing data
+user_avg_rating <- read.table("user_avg_rating.txt")
+movie_raw_info <- read.csv("movie_raw_info.csv")
+sample <- read.csv("testResultForm.csv")
+sample_Y <- sample$rating
+sample$movie_year <- movie_raw_info$year[sample$movie_id]
+sample$movie_cluster <- movie_raw_info$cluster[sample$movie_id]
+sample$movie_avgScore <- movie_raw_info$avgScore[sample$movie_id]
+#TODO: get the right user average rating
+sample$user_avg_rating <- user_avg_rating$V2[sample$user_id]
+sample_X <- sample
+sample_X$movie_id <- NULL
+sample_X$user_id <- NULL
+
+
+#Create training data and testing data from sample data
+#randomly select training data and test data
+index <- sample(nrow(sample_X))
+train_index <- index[1:70000]
+X_train <- sample_X[train_index,]
+Y_train <- sample_Y[train_index]
+
+test_index <- index[70001:98058]
+X_test <- sample_X[test_index,]
+Y_test <- sample_Y[test_index]
 
