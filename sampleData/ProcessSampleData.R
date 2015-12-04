cb011240f4881cb8c24fa674bcd0310e86696d0e
Ray <zhirui1992@gmail.com>
2015-12-04 13:33:46 -0500

    ANN data add External Information

--- sampleData/ProcessSampleData.R
+++ sampleData/ProcessSampleData.R
@@ -1,6 +1,6 @@
 #Process test sample data, 100,000 records
 movie_raw_info <- read.csv("movie_raw_info.csv")
-sample <- read.csv("testSample2.csv")
+sample <- read.csv("testSample3.csv")
 sample_Y <- sample$rating
 
 sample$movie_year <- movie_raw_info$year[sample$movie_id]
@@ -24,11 +24,11 @@ test_index <- index[70001:100000]
 X_test <- sample_X[test_index,]
 Y_test <- sample_Y[test_index]
 
-write.csv(X_train,"testX_train.csv")
-write.csv(X_test,"testX_test.csv")
-write.csv(Y_train,"testY_train.csv")
-write.csv(Y_test,"testY_test.csv")
+write.csv(X_train,"testX_trainE.csv")
+write.csv(X_test,"testX_testE.csv")
+write.csv(Y_train,"testY_trainE.csv")
+write.csv(Y_test,"testY_testE.csv")
 
-write.csv(sample_X,"testSample10_X.csv",row.names = FALSE)
-write.csv(sample_Y,"testSample10_Y.csv",row.names = FALSE)
+write.csv(sample_X,"testSample10E_X.csv",row.names = FALSE)
+write.csv(sample_Y,"testSample10E_Y.csv",row.names = FALSE)
 
